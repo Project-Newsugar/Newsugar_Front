4bd13c30c2e510a6e88a56d39cb40531100963fd
wldsmdl7 <wldmsdl5835@gmail.com>
2025-12-08 13:54:31 +0900

    feat : Navbar, Footer 컴포넌트 생성

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,13 +4,11 @@ import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import OnboardingBasicPage from './pages/OnboardingBasicPage';
 import { MainLayout } from './layout/MainLayout';
-import AlarmListPage from './pages/AlarmListPage';
 
 const router = createBrowserRouter([
 {
     element: <AuthLayout />,
     children: [
-    { path: '/', element: <Navigate to="/login" replace /> },
     { path: '/login', element: <LoginPage /> },
     { path: '/signup', element: <SignupPage /> },
     { path: '/onboarding', element: <OnboardingBasicPage /> },
@@ -19,13 +17,16 @@ const router = createBrowserRouter([
 {
     element: <MainLayout />,
     children: [
-    // { path: '/news', element: <NewsFeedPage /> },
-    { path: '/alarm', element: <AlarmListPage /> },
+      // { path: '/', element: <HomePage /> },
+      // { path: '/categories', element: <CategoriesPage /> },
+      // { path: '/category/:categoryName', element: <CategoryDetailPage /> },
+      // { path: '/news/:newsId', element: <NewsDetailPage /> },
+      { path: '/', element: <div>홈페이지 준비중...</div> }
     ],
 },
 {
     path: '*',
-    element: <Navigate to="/login" replace />,
+    element: <Navigate to="/" replace />,
 },
 ]);
 
