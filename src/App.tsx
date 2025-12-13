50b4740e574fb7e774750377b3a376e0452ad1d4
wldsmdl7 <wldmsdl5835@gmail.com>
2025-12-14 00:57:26 +0900

    feat: 카테고리 별 즐겨찾기 연동 및 목데이터 전체 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import "./App.css";
 import router from "./Router";
 
@@ -17,6 +18,9 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      {import.meta.env.DEV &&
+        <ReactQueryDevtools initialIsOpen={false} />
+      }
     </QueryClientProvider>
   );
 }
